1e1c6046e6cd0230bf36c3f99572521fb57083fd
expecto <expeo@vk.com>
2024-09-02 00:54:44 +0800

    sign in and sign up use cases

---
+++ app/src/main/java/com/imperatorofdwelling/android/domain/usecases/SignInUseCase.kt
@@ -0,0 +1,7 @@
+package com.imperatorofdwelling.android.domain.usecases
+
+class SignInUseCase {
+    fun execute(name: String, password: String): Boolean{
+        return false
+    }
+}
--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/signup/SignUpViewModel.kt
@@ -1,7 +1,13 @@
 package com.imperatorofdwelling.android.presentation.ui.signup
 
 import androidx.lifecycle.ViewModel
+import com.imperatorofdwelling.android.domain.usecases.SignUpUseCase
 
 class SignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val signUpUseCase = SignUpUseCase()
+
+    fun signUp(name: String, email: String, password: String, confirmPassword: String) : Boolean {
+        return signUpUseCase.execute(name, email, password, confirmPassword)
+    }
 }
