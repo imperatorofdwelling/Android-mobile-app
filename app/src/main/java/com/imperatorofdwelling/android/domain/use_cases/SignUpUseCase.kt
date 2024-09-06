9f0e1141f056011f9fb16b521626d34387c9cb66
expecto <expeo@vk.com>
2024-09-06 20:50:00 +0800

    refactoring: named of some packages

--- app/src/main/java/com/imperatorofdwelling/android/domain/use_cases/SignUpUseCase.kt
+++ app/src/main/java/com/imperatorofdwelling/android/domain/use_cases/SignUpUseCase.kt
@@ -1,4 +1,4 @@
-package com.imperatorofdwelling.android.domain.usecases
+package com.imperatorofdwelling.android.domain.use_cases
 
 class SignUpUseCase {
     fun execute(name: String, email: String, password: String, confirmPassword: String): Boolean{
