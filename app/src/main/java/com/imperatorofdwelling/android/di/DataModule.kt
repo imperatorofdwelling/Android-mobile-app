18a2564d6d59f795fa94b3ef9abd8e162cf6bba6
expecto <expeo@vk.com>
2024-09-10 22:17:38 +0800

    fix: hilt modules

--- app/src/main/java/com/imperatorofdwelling/android/di/DataModule.kt
+++ app/src/main/java/com/imperatorofdwelling/android/di/DataModule.kt
@@ -1,6 +1,7 @@
 package com.imperatorofdwelling.android.di
 
 import android.content.Context
+import com.imperatorofdwelling.android.data.local.preferences.SharedPreferencesDataSource
 import com.imperatorofdwelling.android.data.repositories.CitiesRepositoryImpl
 import com.imperatorofdwelling.android.domain.repositories.CitiesRepository
 import dagger.Module
@@ -8,12 +9,18 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
+
     @Provides
-    fun provideCitiesRepository(@ApplicationContext context: Context): CitiesRepository {
-        return CitiesRepositoryImpl(context)
+    @Singleton
+    fun provideCitiesRepository(
+        @ApplicationContext context: Context,
+        sharedPreferencesDataSource: SharedPreferencesDataSource
+    ): CitiesRepository {
+        return CitiesRepositoryImpl(context, sharedPreferencesDataSource)
     }
 }
--- app/src/main/java/com/imperatorofdwelling/android/di/DomainModule.kt
+++ app/src/main/java/com/imperatorofdwelling/android/di/DomainModule.kt
@@ -1,7 +1,11 @@
 package com.imperatorofdwelling.android.di
 
 import com.imperatorofdwelling.android.domain.repositories.CitiesRepository
-import com.imperatorofdwelling.android.domain.use_cases.SearchCityUseCase
+import com.imperatorofdwelling.android.domain.usecases.GetDefaultCityUseCase
+import com.imperatorofdwelling.android.domain.usecases.SearchCityUseCase
+import com.imperatorofdwelling.android.domain.usecases.SetDefaultCityUseCase
+import com.imperatorofdwelling.android.domain.usecases.SignInUseCase
+import com.imperatorofdwelling.android.domain.usecases.SignUpUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +20,29 @@ object DomainModule {
     fun provideSearchCityUseCase(citiesRepository: CitiesRepository): SearchCityUseCase {
         return SearchCityUseCase(citiesRepository)
     }
+
+    @Provides
+    @ViewModelScoped
+    fun provideSignInUseCase(): SignInUseCase{
+        return SignInUseCase()
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideSignUpUseCase(): SignUpUseCase{
+        return SignUpUseCase()
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideSetDefaultCityUseCase(citiesRepository: CitiesRepository): SetDefaultCityUseCase{
+        return SetDefaultCityUseCase(citiesRepository)
+    }
+
+    @Provides
+    @ViewModelScoped
+    fun provideGetDefaultCityUseCase(citiesRepository: CitiesRepository): GetDefaultCityUseCase {
+        return GetDefaultCityUseCase(citiesRepository)
+    }
+
 }
