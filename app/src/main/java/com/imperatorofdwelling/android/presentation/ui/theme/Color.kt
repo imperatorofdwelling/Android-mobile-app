5717363e5f2341e20b10b8c443b59e4aa620b2eb
expecto <expeo@vk.com>
2024-09-30 18:00:06 +0800

    fix: color and type

---
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.imperatorofdwelling.android.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGrey = Color(0xFF131313)
+val Grey1 = Color(0xFF757575)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Accent = Color(0xFF006BE6)
