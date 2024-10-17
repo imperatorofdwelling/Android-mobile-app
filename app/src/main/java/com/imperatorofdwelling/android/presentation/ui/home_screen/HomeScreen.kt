32d4aae1b56351d26c410efde15c791fbc2af62a
expecto <expeo@vk.com>
2024-10-18 05:35:34 +0800

    feat: view model Factory

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/home_screen/HomeScreen.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/home_screen/HomeScreen.kt
@@ -18,14 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.imperatorofdwelling.android.R
-import com.imperatorofdwelling.android.presentation.entities.Currency
 import com.imperatorofdwelling.android.presentation.entities.Dollar
 import com.imperatorofdwelling.android.presentation.entities.Period
 import com.imperatorofdwelling.android.presentation.entities.Price
