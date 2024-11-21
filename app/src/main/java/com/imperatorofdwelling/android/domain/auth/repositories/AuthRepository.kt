6202a2e1e5cb5ed9dab76eb2446ed353bff7f213
Ivan Shinkaruk <expeo@vk.com>
2024-11-21 18:12:52 +0800

    feat: network result data layer

--- app/src/main/java/com/imperatorofdwelling/android/domain/auth/repositories/AuthRepository.kt
+++ app/src/main/java/com/imperatorofdwelling/android/domain/auth/repositories/AuthRepository.kt
@@ -1,7 +1,9 @@
 package com.imperatorofdwelling.android.domain.auth.repositories
 
+import com.imperatorofdwelling.android.domain.auth.entities.NetworkResult
+
 interface AuthRepository {
-    suspend fun register(name: String, email: String, password: String): Boolean
-    suspend fun login(email: String, password: String): Boolean
-    suspend fun recoverPassword(email: String): Boolean
+    suspend fun register(name: String, email: String, password: String): NetworkResult<Boolean>
+    suspend fun login(email: String, password: String): NetworkResult<Boolean>
+    suspend fun recoverPassword(email: String): NetworkResult<Boolean>
 }
