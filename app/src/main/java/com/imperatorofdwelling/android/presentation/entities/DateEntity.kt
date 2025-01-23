15ad9c1b4506cc37d5a5cc3c8cd919f584bc5421
Ivan Shinkaruk <expeo@vk.com>
2025-01-24 04:41:32 +0800

    feat: DateEntity

---
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/entities/DateEntity.kt
@@ -0,0 +1,21 @@
+package com.imperatorofdwelling.android.presentation.entities
+
+data class DateEntity(
+    val day: Int,
+    val month: Int,
+    val year: Int
+) {
+    operator fun compareTo(other: DateEntity): Int {
+        return if (this.year != other.year) {
+            this.year.compareTo(other.year)
+        } else if (this.month != other.month) {
+            this.month.compareTo(other.month)
+        } else {
+            this.day.compareTo(other.day)
+        }
+    }
+
+    override fun toString(): String {
+        return "$day"
+    }
+}
