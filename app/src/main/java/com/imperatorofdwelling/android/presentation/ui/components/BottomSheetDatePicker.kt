02939f72fecbee608e3905e3e058b1ebde5443b9
Ivan Shinkaruk <expeo@vk.com>
2025-01-24 04:42:21 +0800

    feat: DatePicker

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/components/BottomSheetDatePicker.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/components/BottomSheetDatePicker.kt
@@ -3,6 +3,7 @@ package com.imperatorofdwelling.android.presentation.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
@@ -10,7 +11,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.imperatorofdwelling.android.R
@@ -31,19 +34,23 @@ fun BottomSheetDatePicker(
     onFlexibilityClick: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    ModalBottomSheet(onDismissRequest = onDismissRequest, modifier = modifier){
-        Column{
-            Text(text = stringResource(R.string.you_are_booking_for), style = h2)
+    ModalBottomSheet(onDismissRequest = onDismissRequest, modifier = modifier, containerColor = Color.Black) {
+        Text(text = stringResource(R.string.you_are_booking_for), style = h2)
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             DatePicker(
                 selectedYear = selectedYear,
                 selectedMonth = selectedMonth,
                 selectedMonthName = selectedMonthName,
                 onPreviousMonthClick = onPreviousMonthClick,
-                onNextMonthClick = onNextMonthClick
+                onNextMonthClick = onNextMonthClick,
+                onSelectDate = { "" }
             )
         }
         Spacer(modifier = Modifier.height(extraLargeDp))
-        Row{
+        Row {
             Text(text = stringResource(R.string.i_m_flexible), style = forButtons16dp)
             Spacer(modifier = Modifier.width(12.dp))
             Checkbox(checked = flexibility, onCheckedChange = onFlexibilityClick)
