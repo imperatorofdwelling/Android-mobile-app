94742aaf4de2604f93ad4e9a2ba9402e75468909
Ivan Shinkaruk <expeo@vk.com>
2025-02-13 19:08:44 +0800

    feat: error screen

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/components/LoadingUi.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/components/LoadingUi.kt
@@ -1,13 +1,50 @@
 package com.imperatorofdwelling.android.presentation.ui.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.imperatorofdwelling.android.R
+import com.imperatorofdwelling.android.presentation.ui.theme.White
 
 @Composable
 fun LoadingUI() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
+        val infiniteTransition = rememberInfiniteTransition(label = "")
+        val angle by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(2000, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            ), label = ""
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.logo),
+            modifier = Modifier.size(128.dp).rotate(angle),
+            contentDescription = stringResource(R.string.logo),
+            tint = White
+        )
     }
 }
