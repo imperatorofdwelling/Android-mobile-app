7bcbcf40bf5e5bf175bc46370ae00c5611aaf8f0
Ivan Shinkaruk <expeo@vk.com>
2025-03-02 05:12:47 +0800

    fix: navigation

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/navigation/NavigationModel.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/navigation/NavigationModel.kt
@@ -1,10 +1,26 @@
 package com.imperatorofdwelling.android.presentation.ui.navigation
 
+import androidx.lifecycle.viewModelScope
+import com.imperatorofdwelling.android.domain.user.usecases.GetUserRoleUseCase
 import com.imperatorofdwelling.android.presentation.ui.common.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class NavigationModel(
+    private val getUserRoleUseCase: GetUserRoleUseCase
 ): BaseViewModel<NavigationModel.State>(State()) {
+
+    init{
+        updateUserRole()
+    }
+
+    fun updateUserRole() {
+        viewModelScope.launch (Dispatchers.IO){
+            val res = getUserRoleUseCase()
+            _state.update { it.copy(userRole = res) }
+        }
+    }
 
     fun onSetVisible(boolean: Boolean){
         _state.update {
@@ -13,6 +29,7 @@ class NavigationModel(
     }
 
     data class State(
-        val showBottomNavigation: Boolean = true
+        val showBottomNavigation: Boolean = true,
+        val userRole: String = "Tenant"
     )
 }
