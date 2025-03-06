bd949d0c4068e30b59c01d3c539abc0d77235775
Ivan Shinkaruk <expeo@vk.com>
2025-03-06 20:22:06 +0800

    fix: color

--- app/src/main/java/com/imperatorofdwelling/android/domain/user/usecases/GetUserRoleUseCase.kt
+++ app/src/main/java/com/imperatorofdwelling/android/domain/user/usecases/GetUserRoleUseCase.kt
@@ -4,7 +4,7 @@ import com.imperatorofdwelling.android.domain.user.repositories.UserRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetUserRoleUseCase(private val repository: UserRepository) {
-    suspend operator fun invoke(): Flow<String> {
+    suspend operator fun invoke(): Flow<Int> {
         return repository.getUserRole()
     }
 }
