6a1b35530d13f78bb9dbe4e5e707a9e2108bada2
Ivan Shinkaruk <expeo@vk.com>
2025-03-13 20:49:52 +0800

    update: di

--- app/src/main/java/com/imperatorofdwelling/android/di/ViewModelModule.kt
+++ app/src/main/java/com/imperatorofdwelling/android/di/ViewModelModule.kt
@@ -11,6 +11,7 @@ import com.imperatorofdwelling.android.presentation.ui.AuthViewModel
 import com.imperatorofdwelling.android.presentation.ui.edit_profile.EditProfileViewModel
 import com.imperatorofdwelling.android.presentation.ui.home_screen.filtration.FiltrationViewModel
 import com.imperatorofdwelling.android.presentation.ui.landlord.main_screen.MainViewModel
+import com.imperatorofdwelling.android.presentation.ui.landlord.creating.CreatingViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -26,4 +27,5 @@ fun viewModelModule() = module {
     viewModelOf(::EditProfileViewModel)
     viewModelOf(::FiltrationViewModel)
     viewModelOf(::MainViewModel)
+    viewModelOf(::CreatingViewModel)
 }
