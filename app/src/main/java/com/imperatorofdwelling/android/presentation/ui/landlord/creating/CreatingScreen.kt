0c19b15d32812202788da6fb784a2670c4cc9d5e
Ivan Shinkaruk <expeo@vk.com>
2025-04-09 03:46:56 +0800

    update: di

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/CreatingScreen.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/CreatingScreen.kt
@@ -1,111 +1,41 @@
 package com.imperatorofdwelling.android.presentation.ui.landlord.creating
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.key
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import cafe.adriel.voyager.core.screen.Screen
 import com.imperatorofdwelling.android.R
-import com.imperatorofdwelling.android.domain.locations.entities.SearchResult
-import com.imperatorofdwelling.android.presentation.entities.dwelling.Apartment
-import com.imperatorofdwelling.android.presentation.ui.apart_detail.components.MapPoint
-import com.imperatorofdwelling.android.presentation.ui.components.OpenStreetMap
-import com.imperatorofdwelling.android.presentation.ui.components.text_fields.EditTextTrailing
-import com.imperatorofdwelling.android.presentation.ui.home_screen.components.AddressSelection
-import com.imperatorofdwelling.android.presentation.ui.theme.largeDp
+import com.imperatorofdwelling.android.presentation.ui.components.DefaultTopBar
+import com.imperatorofdwelling.android.presentation.ui.landlord.creating.components.Help
 import org.koin.androidx.compose.koinViewModel
-import org.osmdroid.util.GeoPoint
 
 class CreatingScreen : Screen {
     @Composable
     override fun Content() {
         val viewModel = koinViewModel<CreatingViewModel>()
-        val screenState = viewModel.state.collectAsState()
-
+        val state = viewModel.state.collectAsState()
         Scaffold(
             topBar = {
-                TopBar(
-
-                )
-            },
+                AnimatedVisibility(visible = state.value.isInitialScreen) {
+                    DefaultTopBar(title = "")
+                }
+                AnimatedVisibility(visible = !state.value.isInitialScreen) {
+                    DefaultTopBar(title = stringResource(R.string.creating_an_advert))
+                }
+            }
         ) { paddingValues ->
-            BodySelectionAddress(
-                modifier = Modifier.padding(paddingValues),
-                onCitySearch = viewModel::onSearchAddress,
-                searchResult = screenState.value.searchResult ?: emptyList(),
-                onSetAddress = viewModel::setAddress,
-                searchString = screenState.value.searchString,
-                onClickTrailing = viewModel::clearSearchString,
-                currentAddress = screenState.value.address
-            )
+            if (state.value.isInitialScreen) {
+                Help(
+                    modifier = Modifier.padding(paddingValues),
+                )
+            }
         }
     }
-}
-
-@Composable
-private fun TopBar(
-    modifier: Modifier = Modifier,
-) {
 
 }
 
-@Composable
-private fun BodySelectionAddress(
-    modifier: Modifier = Modifier,
-    onCitySearch: (String) -> Unit,
-    searchResult: List<SearchResult?>,
-    searchString: String = "",
-    onClickTrailing: () -> Unit,
-    onSetAddress: (SearchResult) -> Unit,
-    currentAddress: SearchResult? = null
-) {
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-        key(currentAddress) {
-            OpenStreetMap(
-                modifier = Modifier.fillMaxSize(),
-                zoom = 15.0,
-                touchable = true,
-                geoPointCenter = GeoPoint(
-                    currentAddress?.lat ?: 51.50853,
-                    currentAddress?.lon ?: -0.12574
-                ),
-            )
-        }
-        Column(
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 35.dp)
-        ) {
-            EditTextTrailing(
-                modifier = Modifier.width(360.dp),
-                trailingIcon = painterResource(id = R.drawable.cross),
-                onValueChanged = {
-                    onCitySearch(it)
-                },
-                value = searchString,
-                onClickTrailing = onClickTrailing
-            )
-
-            AddressSelection(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                searchResults = searchResult,
-                onAddressClick = onSetAddress,
-            )
-        }
-
-        MapPoint(modifier = Modifier.align(Alignment.Center), dwellingType = Apartment)
-    }
-}
--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/CreatingViewModel.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/CreatingViewModel.kt
@@ -1,84 +1,37 @@
 package com.imperatorofdwelling.android.presentation.ui.landlord.creating
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
-import com.imperatorofdwelling.android.domain.NetworkResult
-import com.imperatorofdwelling.android.domain.locations.entities.SearchResult
-import com.imperatorofdwelling.android.domain.locations.usecases.SearchAddressUseCase
+import com.imperatorofdwelling.android.domain.stays.usecases.GetCreatingHelpUseCase
+import com.imperatorofdwelling.android.domain.stays.usecases.UpdateCreatingHelpUseCase
 import com.imperatorofdwelling.android.presentation.ui.common.BaseViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
 
 class CreatingViewModel(
-    private val searchAddressUseCase: SearchAddressUseCase
+    private val getCreatingHelpUseCase: GetCreatingHelpUseCase,
+    private val updateCreatingHelpUseCase: UpdateCreatingHelpUseCase
 ) : BaseViewModel<CreatingViewModel.State>(State()) {
 
-    private var isSearchingCoolDown = false
-    private var updatingSearch = false
-    private val searchMutex = Mutex()
-    fun onSearchAddress(q: String) {
-        _state.update { it.copy(searchString = q) }
+    init{
+        initState()
+    }
 
-        if (isSearchingCoolDown) {
-            updatingSearch = true
-            return
-        }
-
-        viewModelScope.launch(Dispatchers.IO) {
-            searchMutex.withLock {
-                runCatching {
-                    isSearchingCoolDown = true
-                    when (val result = searchAddressUseCase(_state.value.searchString)) {
-                        is NetworkResult.Success -> {
-                            withContext(Dispatchers.Main) {
-                                _state.update {
-                                    it.copy(searchResult = result.value)
-                                }
-                            }
-                        }
-
-                        is NetworkResult.Error -> {
-                            Log.e("SearchResultError", result.errorMessage)
-                        }
-                    }
-                    delay(1000)
-                    isSearchingCoolDown = false
-                    if(updatingSearch){
-                        updatingSearch = false
-                        onSearchAddress(_state.value.searchString)
-                    }
-                }.onFailure { e ->
-                    Log.e("ServerError", e.message.toString())
+    private fun initState(){
+        viewModelScope.launch(Dispatchers.IO){
+            getCreatingHelpUseCase().collect{ value ->
+                _state.update{
+                    it.copy(isInitialScreen = value)
                 }
             }
         }
     }
 
-    fun setAddress(address: SearchResult) {
-        _state.update {
-            it.copy(address = address)
-        }
-    }
-
-    fun updateShowAddressSelection(showAddressSelection: Boolean) {
-        _state.update {
-            it.copy(showAddressSelection = showAddressSelection)
-        }
-    }
-
-    fun clearSearchString(){
-        _state.update { it.copy(searchString = "") }
+    fun updateCreatingHelpScreen(value: Boolean){
+        updateCreatingHelpUseCase(value)
     }
 
     data class State(
-        val searchString: String = "",
-        val searchResult: List<SearchResult?>? = null,
-        val showAddressSelection: Boolean = false,
-        val address: SearchResult? = null,
+        val isInitialScreen: Boolean = false
     )
 }
