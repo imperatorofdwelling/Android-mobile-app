8122b7944150eb3d6335965bab4cbdeef5e69345
Ivan Shinkaruk <expeo@vk.com>
2025-04-19 05:53:32 +0800

    fix: step counter

--- app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/components/StepCounter.kt
+++ app/src/main/java/com/imperatorofdwelling/android/presentation/ui/landlord/creating/components/StepCounter.kt
@@ -5,23 +5,42 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.imperatorofdwelling.android.presentation.ui.theme.Accent
 import com.imperatorofdwelling.android.presentation.ui.theme.White
+
+@Preview
+@Composable
+fun StepCounterPreview() {
+    StepCounter(
+        Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+    )
+}
 
 @Composable
 fun StepCounter(
     modifier: Modifier = Modifier,
     step: Int = 1,
 ) {
-    Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
         repeat(4) { index ->
             Box(
                 modifier = Modifier
-                    .background(color = if (index <= step - 1) Accent else White)
-                    .fillMaxWidth(0.2f)
+                    .weight(1f)
+                    .height(4.dp)
+                    .background(
+                        color = if (index <= step - 1) Accent else White
+                    )
             )
         }
     }
 }
+
